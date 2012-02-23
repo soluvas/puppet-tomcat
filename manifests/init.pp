bb69da47d9a02132a71c36b920a9683dbe079108
Hendy Irawan <hendy@soluvas.com>
2012-02-23 21:51:58 +0700

    Working Tomcat :)

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,8 @@
 # class { tomcat: version => 6 }
 # [Remember: No empty lines between comments and class definition]
 class tomcat (
-	$version
+	$version,
+	$java_home = '/usr/lib/jvm/java-1.7.0-openjdk-amd64'
 ) {
 
   $package = "tomcat${version}"
@@ -29,12 +30,20 @@ class tomcat (
   	'tomcat': name => $package, ensure => present;
   	'tomcat-admin': name => "${package}-admin", ensure => present;
   }
+  exec { tomcat_java_home:
+  	command => "sed -i -e '/JAVA_HOME/ c JAVA_HOME=\"${java_home}\"' /etc/default/tomcat6",
+  	path => ['/bin', '/usr/bin'],
+  	logoutput => true,
+  	unless => "grep '${java_home}' /etc/default/tomcat6",
+  	require => Package['tomcat'],
+  	notify => Service['tomcat']
+  }
   service { 'tomcat':
   	name => $package,
   	enable => true,
   	ensure => running,
   	hasstatus => true,
-  	require => Package['tomcat']
+  	require => [ Package['tomcat'], Exec['tomcat_java_home'] ]
   }
 
 }
